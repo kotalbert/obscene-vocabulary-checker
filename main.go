09181ea39c283b4f54bf6f84401ebe57157e223a
Pawel Daniluk <pawel.daniluk@kotalbert.net>
2024-03-01 21:01:01 +0100

    Merge pull request #1
    * Implement read-file operation

--- main.go
+++ main.go
@@ -1,5 +1,45 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
 func main() {
-	// write your code here
+	fn := readUserInput()
+	ws := readLines(fn)
+
+	for _, w := range ws {
+		fmt.Println(w)
+	}
+}
+
+func readUserInput() string {
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return ""
+	}
+	return input
+}
+
+// readLines reads a file into a slice of strings, separated by newlines.
+func readLines(filename string) []string {
+	file, err := os.OpenFile(filename, os.O_RDONLY, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		_ = file.Close()
+	}(file)
+
+	var ls []string
+	s := bufio.NewScanner(file)
+	for s.Scan() {
+		ls = append(ls, s.Text())
+	}
+	return ls
+
 }
